36c20b0f8307bc922c5c760a60b7b10e5f9132d2
Ciaran Flanagan <ciaran@cicisoft.net>
2024-12-14 14:43:16 +0000

    Day 3 - Part 2

--- day3/day3.js
+++ day3/day3.js
@@ -20,4 +20,28 @@ function decode(input) {
     return total;
 }
 
-console.log(decode(input));
+function decodePart2(input) {
+    const regex = /mul\(\d+,\d+\)|do\(\)|don't\(\)/g; // Matches mul(X,Y), do() and don't(), where X and Y are integers
+    let total = 0;
+
+    let matches = input.match(regex);
+    if (matches) {
+        let status = 'enabled';
+
+        matches.forEach(match => {
+            if (match.includes('do')) {
+                status = (match === 'do()') ? 'enabled' : 'disabled';
+                return;
+            }
+            if (status === 'disabled') return;
+            
+            let nums = match.match(/\d+/g);
+            total += parseInt(nums[0]) * parseInt(nums[1]); 
+        });
+    }
+
+    return total;
+}
+
+console.log('Part 1: ' + decode(input));
+console.log('Part 2: ' + decodePart2(input));
