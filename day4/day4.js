f3c91a34eaf01deefd5af868bfa76b3a49c35dea
Ciaran Flanagan <ciaran@cicisoft.net>
2024-12-14 18:59:21 +0000

    Day 4 - Part 1

---
+++ day4/day4.js
@@ -0,0 +1,52 @@
+/**
+ * Day 1: Compare distance between two sorted lists
+ */
+
+const input = require('fs').readFileSync('../input/day4.txt', 'utf8')
+const table = input.split('\n');
+
+const directions = [
+    { x: 0, y: 1 },   // right
+    { x: 0, y: -1 },  // left
+    { x: -1, y: 0 },  // up
+    { x: 1, y: 0 },   // down
+    { x: 1, y: 1 },   // down-right
+    { x: 1, y: -1 },  // down-left
+    { x: -1, y: 1 },  // up-right
+    { x: -1, y: -1 }  // up-left
+];
+const word = 'XMAS';
+
+function findWord(x, y, direction) {
+    for (let i = 0; i < word.length; i++) {
+        const newX = x + i * direction.x;
+        const newY = y + i * direction.y;
+
+        // Out of bounds
+        if (newX < 0 || newX >= table.length
+        || newY < 0 || newY >= table[0].length) return false;
+        
+        // Word not found
+        if (table[newX][newY] !== word[i]) return false;
+    }
+
+    return true;
+}
+
+function traverseTable() {
+    let count = 0;
+
+    for (let row = 0; row < table.length; row++) {
+        for (let col = 0; col < table[row].length; col++) {
+            for (const direction of directions) {
+                if (findWord(row, col, direction)) {
+                    count++;
+                }
+            }
+        }
+    }
+
+    return count;
+}
+
+console.log(traverseTable());
